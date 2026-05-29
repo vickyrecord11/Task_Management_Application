1e7fa847a51ed155626f906421cb9a309dcfb7eb
Vignesh K <vickyrecord11@gmail.com>
2026-05-29 21:30:22 +0530

    Add tasks using POSTMAN

--- task-management-application-ui/script.js
+++ task-management-application-ui/script.js
@@ -1,6 +1,6 @@
 // let editRow = null;
 
-// let editId = null;
+ //let editId = null;
 
 
 // function showForm() {
@@ -103,6 +103,10 @@
 
 //             document.getElementById("taskForm").reset();
 
+//         });
+
+//     }
+
 //             document.getElementById("taskForm").style.display =
 //                 "none";
 //         });
@@ -153,152 +157,60 @@
 // }
 
 
-// function loadTasks() {
+function loadTasks() {
 
-//     fetch("http://localhost:8000/tasks")
+    fetch("http://localhost:8000/tasks")
 
-//         .then(response => response.json())
+        .then(response => response.json())
 
-//         .then(result => {
+        .then(result => {
 
-//             let tasks = result.data;
+            let tasks = result.data;
 
-//             let table =
-//                 document.getElementById("taskTable")
-//                     .getElementsByTagName("tbody")[0];
+            const tasksTableBody =
+                document.getElementById("tasksTableBody");
 
-//             table.innerHTML = "";
+            tasksTableBody.innerHTML = "";
 
-//             tasks.forEach((task, index) => {
+            tasks.forEach((task, index) => {
 
-//                 let row = table.insertRow();
+                let row = tasksTableBody.insertRow();
 
-//                 row.innerHTML = `
+                row.innerHTML = `
 
-//                 <td>${index + 1}</td>
+                <td>${index + 1}</td>
 
-//                 <td>${task.name}</td>
+                <td>${task.name}</td>
 
-//                 <td>${task.category}</td>
+                <td>${task.category}</td>
 
-//                 <td>${task.description}</td>
+                <td>${task.description}</td>
 
-//                 <td>${task.priority}</td>
+                <td>${task.dueDate}</td>
 
-//                 <td>${task.dueDate}</td>
-
-//                 <td>${task.status}</td>
-
-//                 <td>
-
-//                     <button class="edit-btn"
-//                         onclick='editTask(${JSON.stringify(task)})'>
-//                         Edit
-//                     </button>
-
-//                     <button class="delete-btn"
-//                         onclick="deleteTask(${task.id})">
-//                         Delete
-//                     </button>
-
-//                 </td>
-//             `;
-//             });
-//         });
-// }
-
-// loadTasks();
-const tasksTableBody = document.getElementById("tasksTableBody");
-
-const tasks = [
-
-    {
-        name: "UI Design",
-        category: "Frontend",
-        description: "Create dashboard layout",
-        dueDate: "12 Aug 2025",
-        status: "In Progress",
-        priority: "High"
-    },
-
-    {
-        name: "Bug Fixing",
-        category: "Testing",
-        description: "Fix alignment and UI issues",
-        dueDate: "18 Aug 2025",
-        status: "Done",
-        priority: "Low"
-    },
-
-    {
-        name: "API Integration",
-        category: "Backend",
-        description: "Connect frontend with API",
-        dueDate: "20 Aug 2025",
-        status: "To Do",
-        priority: "High"
-    },
-
-    {
-        name: "Database Setup",
-        category: "Database",
-        description: "Configure MySQL database",
-        dueDate: "22 Aug 2025",
-        status: "In Progress",
-        priority: "Medium"
-    },
-
-    {
-        name: "Testing Module",
-        category: "QA",
-        description: "Test all application modules",
-        dueDate: "25 Aug 2025",
-        status: "Done",
-        priority: "Low"
-    }
-
-];
-
-
-tasks.forEach((task, index) => {
-
-    tasksTableBody.innerHTML += `
-
-        <tr>
-
-            <td>${index + 1}</td>
-
-            <td>${task.name}</td>
-
-            <td>${task.category}</td>
-
-            <td>${task.description}</td>
-
-            <td>${task.dueDate}</td>
-
-            <td>
-
+                <td>
+                
                 <span class="status-pill progress-pill">
-
-                    ${task.status}
-
-                </span>
-
-            </td>
-
-            <td>
-
+                
+                    ${task.status} 
+                
+                </span> 
+                
+                </td> 
+                
+                <td> 
+                
                 <span class="priority-pill high-pill">
-
-                    ${task.priority}
-
+                
+                    ${task.priority} 
+                
                 </span>
+                
+                </td>
 
-            </td>
+                <td>
 
-            <td>
-
-                <div class="action-buttons">
+                    <div class="action-buttons">
 
                     <button class="edit-btn">
 
@@ -316,11 +228,126 @@ tasks.forEach((task, index) => {
 
                 </div>
 
-            </td>
+                </td>
+            `;
+            });
+        });
+}
 
-        </tr>
+loadTasks();
+// const tasksTableBody = document.getElementById("tasksTableBody");
 
-    `;
+// const tasks = [
 
-});
+//     {
+//         name: "UI Design",
+//         category: "Frontend",
+//         description: "Create dashboard layout",
+//         dueDate: "12 Aug 2025",
+//         status: "In Progress",
+//         priority: "High"
+//     },
+
+//     {
+//         name: "Bug Fixing",
+//         category: "Testing",
+//         description: "Fix alignment and UI issues",
+//         dueDate: "18 Aug 2025",
+//         status: "Done",
+//         priority: "Low"
+//     },
+
+//     {
+//         name: "API Integration",
+//         category: "Backend",
+//         description: "Connect frontend with API",
+//         dueDate: "20 Aug 2025",
+//         status: "To Do",
+//         priority: "High"
+//     },
+
+//     {
+//         name: "Database Setup",
+//         category: "Database",
+//         description: "Configure MySQL database",
+//         dueDate: "22 Aug 2025",
+//         status: "In Progress",
+//         priority: "Medium"
+//     },
+
+//     {
+//         name: "Testing Module",
+//         category: "QA",
+//         description: "Test all application modules",
+//         dueDate: "25 Aug 2025",
+//         status: "Done",
+//         priority: "Low"
+//     }
+
+// ];
+
+
+// tasks.forEach((task, index) => {
+
+//     tasksTableBody.innerHTML += `
+
+//         <tr>
+
+//             <td>${index + 1}</td>
+
+//             <td>${task.name}</td>
+
+//             <td>${task.category}</td>
+
+//             <td>${task.description}</td>
+
+//             <td>${task.dueDate}</td>
+
+//             <td>
+
+//                 <span class="status-pill progress-pill">
+
+//                     ${task.status}
+
+//                 </span>
+
+//             </td>
+
+//             <td>
+
+//                 <span class="priority-pill high-pill">
+
+//                     ${task.priority}
+
+//                 </span>
+
+//             </td>
+
+//             <td>
+
+//                 <div class="action-buttons">
+
+//                     <button class="edit-btn">
+
+//                         <img src="edit-svgrepo-com.svg"
+//                              class="action-icon">
+
+//                     </button>
+
+//                     <button class="delete-btn">
+
+//                         <img src="delete-svgrepo-com.svg"
+//                              class="action-icon">
+
+//                     </button>
+
+//                 </div>
+
+//             </td>
+
+//         </tr>
+
+//     `;
+
+// });
 
